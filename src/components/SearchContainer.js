d3cf8f3d87f305f266ebd3649b4465ef07f68fe5
Alejandro Garcia <thinkingmono92@gmail.com>
2023-12-11 10:10:49 -0500

    SearchContainer.js | declare localSearch state variable using useState hook. declare debounce function to set LocalSearch value taking value from search filter's field, then clear time out, and after 1 sec call dispatch to run handleChange and send search field value to the allJobs slice. declare optimizedDebounce using useMemo hook, to call debounce function on search field onChange event. if form field value changes call debounce function and send request to API, if not doesn't send request and search results doesn't change.

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -2,25 +2,42 @@ import { useDispatch, useSelector } from 'react-redux'
 import Wrapper from '../assets/wrappers/SearchContainer'
 import { FormRow, FormRowSelect } from './'
 import { clearFilters, handleChange } from '../features/allJobs/allJobsSlice';
+import { useMemo, useState } from 'react';
 
 const SearchContainer = () => {
     const { isLoading, search, searchStatus, searchType, sort, sortOptions } = useSelector((store) => store.allJobs);
     const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
     const dispatch = useDispatch();
+    const [localSearch, setLocalSearch] = useState('');
+
+    const debounce = () => {
+        let timeoutId;
+        return (e) => {
+            setLocalSearch(e.target.value);
+            clearTimeout(timeoutId);
+            timeoutId = setTimeout(() => {
+                dispatch(handleChange({ name: e.target.name, value: e.target.value }));
+            }, 1000);
+        }
+    }
+
     const handleSearch = (e) => {
-        if (isLoading) return;
         dispatch(handleChange({ name: e.target.name, value: e.target.value }));
     }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         dispatch(clearFilters());
     }
+
+    const optimizedDebounce = useMemo(() => debounce(), []);
+
     return (
         <Wrapper>
             <form className="form">
                 <h4>Search Form</h4>
                 <div className="form-center">
-                    <FormRow type='text' name='search' value={search} onChange={handleSearch} />
+                    <FormRow type='text' name='search' value={localSearch} onChange={optimizedDebounce} />
                     <FormRowSelect labelText='status' name='searchStatus' value={searchStatus} onChange={handleSearch} list={['all', ...statusOptions]} />
                     <FormRowSelect labelText='type' name='searchType' value={searchType} onChange={handleSearch} list={['all', ...jobTypeOptions]} />
                     <FormRowSelect name='sort' value={sort} onChange={handleSearch} list={sortOptions} />
